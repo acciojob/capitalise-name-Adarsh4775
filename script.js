1b1b219c48440223cda351bab67e6c864b81b893
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-21 18:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const theName = document.getElementById("fname");
+
+theName.addEventListener("blur" function(){
+	theName.value = theName.value.toUpperCase();
+});
